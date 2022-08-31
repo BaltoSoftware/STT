7dcd812141b725818a98d923deb9ec2cabf84864
Alex Estes <alex.estes@baltosoftware.com>
2022-08-31 19:03:55 +0000

    working interpolation script

--- data/lm/interpolate_lm.py
+++ data/lm/interpolate_lm.py
@@ -13,7 +13,7 @@ def convert_text(idx, input_txt, output_dir):
 
     data_lower = os.path.join(output_dir, "{}_lower.txt.gz".format(idx))
 
-    print("\nConverting to lowercase and counting word occurrences ...")
+    print("\nConverting to lowercase ...")
     with io.TextIOWrapper(
         io.BufferedWriter(gzip.open(data_lower, "w+")), encoding="utf-8"
     ) as file_out:
@@ -29,7 +29,6 @@ def convert_text(idx, input_txt, output_dir):
 
         for line in progressbar.progressbar(file_in):
             line_lower = line.lower()
-            counter.update(line_lower.split())
             file_out.write(line_lower)
 
         file_in.close()
@@ -67,7 +66,7 @@ def build_intermediate_lm(args, idx, data_lower):
 
 def binarize_lm(args, interpolated_lm_arpa):
 
-	# Quantize and produce trie binary.
+    # Quantize and produce trie binary.
     print("\nBuilding interpolated_lm.binary ...")
     binary_path = os.path.join(args.output_dir, "interpolated_lm.binary")
     subprocess.check_call(
@@ -90,6 +89,16 @@ def main():
         description="Generate an interpolated language model from multiple corpora."
     )
     parser.add_argument(
+        "--name",
+        help="Name to use for writing interpolated LM files",
+        type=str,
+        required=True,
+        nargs=1
+    )
+    parser.add_argument(
+        "--output_dir", help="Directory path for the output", type=str, required=True
+    )
+    parser.add_argument(
         "--input_txts",
         help="Paths to file.txt or file.txt.gz with sample sentences",
         type=str,
@@ -102,13 +111,6 @@ def main():
         type=str,
         required=True,
         nargs="+"
-    )
-    parser.add_argument(
-        "--name",
-        help="Name to use for writing interpolated LM files",
-        type=str,
-        required=True
-        nargs=1
     )
     parser.add_argument(
         "--kenlm_bins",
@@ -153,32 +155,43 @@ def main():
         required=True,
     )
 
-	args = parser.parse_args()
-	assert len(args.input_txts) == len(args.weights)
+    args = parser.parse_args()
+    assert len(args.input_txts) == len(args.weights)
 
-	intermediate_lms = []
+    intermediate_lms = []
 
-	for idx, vals in enumerate(zip(args.input_txts, args.weights)):
-		input_txt, weight = vals
-		data_lower = convert_text(idx, input_txt, args.output_dir)
-		intermediate_lm_path = build_intermediate_lm(args, idx, data_lower)
-		intermediate_lms.append(intermediate_lm_path)
+    for idx, vals in enumerate(zip(args.input_txts, args.weights)):
+        input_txt, weight = vals
+        data_lower = convert_text(idx, input_txt, args.output_dir)
+        intermediate_lm_path = build_intermediate_lm(args, idx, data_lower)
+        intermediate_lms.append(intermediate_lm_path)
 
-	interpolated_lm_arpa = os.path.join(output_dir, "{}.arpa".format(args.name))
+    interpolated_lm_arpa = os.path.join(args.output_dir, "{}.arpa".format(args.name[0]))
 
-	subargs = [
-		os.path.join(args.kenlm_bins, "interpolate"),
-		"--models",
-		" ".join(intermediate_lms),
-		"--weights",
-		" ".join(args.weights),
-	]
+    subargs = [
+        os.path.join(args.kenlm_bins, "interpolate"),
+        "-m",
+        " ".join(intermediate_lms),
+        "-w",
+        " ".join(args.weights),
+    ]
+ 
+    interpolate_cmd = "{} -m {} -w {}".format(
+        os.path.join(args.kenlm_bins, "interpolate"),
+        " ".join(intermediate_lms),
+        " ".join(args.weights)
+        )
 
-	subprocess.check_call(subargs, stdout=interpolated_lm_arpa)
+    print(interpolate_cmd)
+    with open(interpolated_lm_arpa, "w") as f:
+        #subprocess.check_call(subargs, shell=True,  stdout=f)
+        subprocess.check_call(interpolate_cmd, shell=True, stdout=f)
 
-	# will skip arpa file filtering for now
-	# need some logic to include all ngrams from customer data
-	# and filter out ngrams past some threshold for other data
+    # will skip arpa file filtering for now
+    # need some logic to include all ngrams from customer data
+    # and filter out ngrams past some threshold for other data
 
-	binarize_lm(args, interpolated_lm_arpa)
+    binarize_lm(args, interpolated_lm_arpa)
 
+if __name__ == "__main__":
+    main()
